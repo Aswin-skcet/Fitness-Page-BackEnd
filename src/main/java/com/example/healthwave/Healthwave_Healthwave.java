d554663ad0134c652a797610546d57dd65e6ae95
Aswin-skcet <151620301+Aswin-skcet@users.noreply.github.com>
2024-08-15 14:37:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/healthwave/Healthwave_Healthwave.java
@@ -0,0 +1,13 @@
+package com.example.healthwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Healthwave_Healthwave {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Healthwave_Healthwave.class, args);
+	}
+
+}
